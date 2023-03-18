802a2f8d3b242b05357f9c1524affd341a43d127
Maxim <334890@gmail.com>
2023-03-18 15:29:22 +1000

    Add aux autofilling functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 //
+const form = document.querySelector("form");
+const formFields = document.querySelectorAll("form input");
+
 window.onload = () => {
   //const qrButton = document.querySelector(".show-qrcode");
   const fillButton = document.querySelector(".fill-form");
   //qrButton.addEventListener("click", showQRCode);
   fillButton.addEventListener("click", fillForm);
+  form.addEventListener("input", handleInput);
+  restoreForm();
 };
 
 /* function showQRCode() {
@@ -12,9 +17,53 @@ window.onload = () => {
 
 function fillForm(event) {
   console.log(event);
-  if (event.ctrlKey) {
-    console.log("save form to localStorage");
+  if (event.ctrlKey && event.shiftKey) {
+    console.log("clearing form");
+    formFields.forEach((field) => {
+      field.value = "";
+    });
+  } else if (event.ctrlKey) {
+    console.log("saving whole form to localStorage");
+    saveToStorage();
+  } else if (event.shiftKey) {
+    console.log("clearing localStorage fields");
+    const formData = localStorage.getItem("formData");
+    localStorage.clear();
+    localStorage.setItem("formData", formData);
   } else {
-    console.log("fill form from localStorage");
+    console.log("filling form from localStorage");
+    loadFromStorage();
   }
+}
+
+function saveToStorage() {
+  const data = {};
+  //const formFields = document.querySelectorAll("form input");
+  formFields.forEach((field) => {
+    data[field.id] = field.value;
+  });
+  console.log(data);
+  localStorage.setItem("formData", JSON.stringify(data));
+}
+
+function loadFromStorage() {
+  const data = JSON.parse(localStorage.getItem("formData"));
+  //const formFields = document.querySelectorAll("form input");
+  formFields.forEach((field) => {
+    const key = field.id;
+    field.value = data[key];
+  });
+}
+
+function handleInput(event) {
+  console.log(event);
+  console.log(event.target.value);
+  localStorage.setItem(event.target.id, event.target.value);
+}
+
+function restoreForm() {
+  formFields.forEach((field) => {
+    const key = field.id;
+    field.value = localStorage.getItem(key);
+  });
 }
