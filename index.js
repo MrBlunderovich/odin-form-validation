9593e9762ccbd92120e60868bdfc2b8f9088a5ef
Maxim <334890@gmail.com>
2023-03-18 13:30:09 +1000

    Make form fields

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //
+window.onload = () => {
+  //const qrButton = document.querySelector(".show-qrcode");
+  const fillButton = document.querySelector(".fill-form");
+  //qrButton.addEventListener("click", showQRCode);
+  fillButton.addEventListener("click", fillForm);
+};
+
+/* function showQRCode() {
+  console.log("that would need Webpack");
+} */
+
+function fillForm(event) {
+  console.log(event);
+  if (event.ctrlKey) {
+    console.log("save form to localStorage");
+  } else {
+    console.log("fill form from localStorage");
+  }
+}
